7b141c106bdf478440943468cd1db6b4f69b10e3
CodeWithKrish <codewithkrish1729@gmail.com>
2024-07-13 15:37:31 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,42 +1,79 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
 
-// Function to play next sequence
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  // Flash animation and play sound
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
 
-// Function to play sound for a given color
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
 function playSound(name) {
-  var audio = new Audio(name + ".mp3");
+  var audio = new Audio("/" + name + ".mp3");
   audio.play();
 }
-function playSound(name) {
-    var audio = new Audio(name + ".mp3");
-    audio.play();
-  }
-  function playSound(name) {
-    var audio = new Audio(name + ".mp3");
-    audio.play();
-  }
-  function playSound(name) {
-    var audio = new Audio(name + ".mp3");
-    audio.play();
-  }
-      
 
-// Event handler for button clicks
-$("button").on("click", function() {
-  var userChosenColour = $(this).attr("id");
-  playSound(userChosenColour);
-  // You can add more logic here for checking user input against gamePattern
-});
-
-// Example usage:
-nextSequence(); // Call nextSequence to start the game
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
